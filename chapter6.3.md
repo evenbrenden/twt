36bcca0af6b3777c79d42d9cfde4844a3e73e07d
Even Brenden <evenbrenden@gmail.com>
2022-04-03 12:02:41 +0200

    Add chapter 6 exercises

---
+++ chapter6.3.md
@@ -0,0 +1,34 @@
+# Chapter 6.3
+
+> **Exercise 6.3-i**
+>
+> What is the rank of `Int -> forall a. a -> a`? Hint: try adding the explicit parentheses.
+
+```
+Int -> forall a. a -> a ~
+Int -> forall a. (a -> a)
+```
+
+Deepest `forall` has 1 arrow to the left of it, so it is rank-1.
+
+> **Exercise 6.3-ii**
+>
+> What is the rank of `(a -> b) -> (forall c. c -> a) -> b`? Hint: recall that the function arrow is right-associative, so `a -> b -> c` is actually parsed as `a -> (b -> c)`.
+
+```
+(a -> b) -> (forall c. c -> a) -> b ~
+forall a b. ((a -> b) -> ((forall c. c -> a) -> b))
+```
+
+Deepest `forall` has 2 arrows to the left of it, so it is rank-2.
+
+> **Exercise 6.3-iii**
+>
+> What is the rank of `((forall x. m x -> b (z m x)) -> b (z m a)) -> m a`? Believe it or not, this is a real type signature we had to write back in the bad old days before `MonadUnliftIO`!
+
+```
+((forall x. m x -> b (z m x)) -> b (z m a)) -> m a ~
+forall m b z a. (((forall x. m x -> b (z m x)) -> b (z m a)) -> m a)
+```
+
+Deepest `forall` has 3 arrows to the left of it, so it is rank-3.
---
+++ chapter6.4.hs
@@ -0,0 +1,54 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -p ghc ghcid -i "ghcid -c 'ghci -Wall'"
+
+{-# LANGUAGE RankNTypes #-}
+
+newtype Cont a = Cont
+    { unCont :: forall r. (a -> r) -> r
+    }
+
+-- Exercise 6.4-i
+-- Provide a Functor instance for Cont. Hint: use lots of type holes, and an explicit lambda whenever looking for a function type. The implementation is sufficiently difficult that trying to write it point-free will be particularly mind-bending.
+
+instance Functor Cont where
+    fmap f c = Cont $ \ar -> unCont c (ar . f)
+
+-- Exercise 6.4-ii
+-- Provide the Applicative instances for Cont.
+
+instance Applicative Cont where
+    pure a = Cont $ \ar -> ar a
+    cf <*> ca =
+        let abrr = unCont cf
+            arr  = unCont ca
+        in  Cont $ \br -> abrr $ \ab -> arr $ br . ab
+
+-- Exercise 6.4-iii
+-- Provide the Monad instances for Cont.
+
+instance Monad Cont where
+    return = pure
+    ma >>= amb =
+        let arr = unCont ma in Cont $ \br -> arr (\a -> unCont (amb a) br)
+
+-- Exercise 6.4-iv
+-- There is also a monad transformer version of Cont. Implement it.
+
+newtype ContT m a = ContT
+    { unContT :: forall r. (a -> m r) -> m r
+    }
+
+instance Monad m => Functor (ContT m) where
+    fmap f c = ContT $ \ar -> unContT c (ar . f)
+
+instance Monad m => Applicative (ContT m) where
+    pure a = ContT $ \ar -> ar a
+    cf <*> ca =
+        let abrr = unContT cf
+            arr  = unContT ca
+        in  ContT $ \br -> abrr $ \ab -> arr $ br . ab
+
+instance Monad m => Monad (ContT m) where
+    return = pure
+    ma >>= amb =
+        let arr = unContT ma in ContT $ \br -> arr (\a -> unContT (amb a) br)
