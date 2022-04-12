ee7ebbd0c2d8d321d02d24b704dcfd1471d89425
Even Brenden <evenbrenden@gmail.com>
2022-04-12 17:41:31 +0200

    HasPrintf w/o Proxy

---
+++ printf-no-proxy.hs
@@ -0,0 +1,43 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -p ghc ghcid -i "ghcid -c 'ghci -Wall'"
+
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+import           Data.Kind                      ( Type )
+import           Data.Proxy                     ( Proxy(..) )
+import           GHC.TypeLits
+
+data (a :: k1) :<< (b :: k2)
+infixr 5 :<<
+
+class HasPrintf a where
+    type Printf a :: Type
+    format :: String -> Printf a
+
+instance KnownSymbol text => HasPrintf (text :: Symbol) where
+    type Printf text = String
+    format s = s <> (symbolVal (Proxy @text))
+
+instance (HasPrintf a, KnownSymbol text) => HasPrintf ((text :: Symbol) :<< a) where
+    type Printf (text :<< a) = Printf a
+    format s = format @a (s <> symbolVal (Proxy @text))
+
+instance (HasPrintf a, Show param) => HasPrintf ((param :: Type) :<< a) where
+    type Printf (param :<< a) = param -> Printf a
+    format s param = format @a (s <> show param)
+
+instance {-# OVERLAPPING #-} HasPrintf a => HasPrintf (String :<< a) where
+    type Printf (String :<< a) = String -> Printf a
+    format s param = format @a (s <> param)
+
+example :: String
+example = format @(Int :<< "+" :<< Int :<< "=3") mempty 1 2
