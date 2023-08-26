2396ff51b8bde7abf39b463ef64375e0655b3cfd
Even Brenden <evenbrenden@users.noreply.github.com>
2023-08-26 10:37:58 +0200

    Pin Nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,6 +1,12 @@
-with import <nixpkgs> { };
+{ }:
 
-mkShell {
+let
+  pkgs = import (builtins.fetchTarball {
+    name = "nixos-21.11";
+    url = "https://github.com/nixos/nixpkgs/archive/8e6b3914626900ad8f465c3e3541edbb86a95d41.tar.gz";
+    sha256 = "02smga9l709cg3almbjk1jf6jnl1x5lnqwv0jz6d5rn83c3n7dc0";
+  }) { };
+in pkgs.mkShell {
   buildInputs = with pkgs; [
     ghcid
     (haskellPackages.ghcWithPackages (p: [
